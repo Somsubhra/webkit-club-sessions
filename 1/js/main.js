a402546942539c58b99b5fb02c2f643c32ec020a
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-08-13 12:24:20 +0530

    Echo json data

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,7 +1,12 @@
 var apiRoot = "http://localhost:8080/api";
 
-$(function() {
+function fetchScores() {
 	$.getJSON(apiRoot, function(data) {
-		console.log(data);
-	})
+		console.log(data[0].name + ":" + data[0].runs);
+		console.log(data[1].name + ":" + data[1].runs);
+	});
+}
+
+$(function() {
+	setInterval(fetchScores, 1000);
 });
